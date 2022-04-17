f2a58b3ca4fa9c9715d5aa6c8914217c212bde4c
Alex Barrachina <alejba02@ucm.es>
2022-04-17 10:56:49 +0200

    primer llenado de pokemons

--- pokemon-scrapper/Main.rb
+++ pokemon-scrapper/Main.rb
@@ -12,25 +12,28 @@ def parseForma(forma)
         return "alola"
     else
         return forma
-
+    end
 end
 
 def creaURLForma(nombre)
+    if(name == "ho-oh")
+        return nombre 
+    end
     split = nombre.split("-")
     nombre = split[1]
-    forma = split[0]
+    forma = parseForma(split[0])
     exceso = ""
-    if(lenght(split) == 3)
+    if(split.length() == 3)
         exceso = split[2]
     end
     url = ""
     if(nombre == "mime")
         url = forma + "-" + nombre
     elsif (nombre == "mr")
-        url = nombre + "-" + exceso + parseForma(forma)
+        url = nombre + "-" + exceso + "-" + forma
     else
-        url = nombre + "-" parseForma(forma)
-        if(lenght(split) == 3)
+        url = nombre + "-" + forma
+        if(split.length() == 3)
             url += "-" + exceso
         end
     end
@@ -38,22 +41,30 @@ def creaURLForma(nombre)
 end
 
 def createPokemonFile(filename, gen)
-  file = File.open(filename, "r")
-  data = file_data = file.readlines.map(&:chomp)
-  data.each{ |poke|
-        if(lenght(poke.split("-") >= 2)
+    file = File.open(filename, "r")
+    data = file_data = file.readlines.map(&:chomp)
+    data.each{ |poke|
+        if(File.file?("../Resources/#{gen}/#{poke}.txt"))
+            next
+        end
+        if(poke.split("-").length >= 2)
             poke = creaURLForma(poke)
         end
         agent = Mechanize.new 
         url = "https://pokeapi.co/api/v2/pokemon/"
-        page = agent.get(url + poke + "/")
+        begin
+            page = agent.get(url + poke + "/")
+        rescue Mechanize::ResponseCodeError => e
+            puts "Falta #{poke}"
+            next 
+        end
         p = JSON.parse(page.content)
         type1 = p["types"][0]["type"]["name"]
         type2 = "None"
         begin
             type2 = p["types"][1]["type"]["name"]
         rescue NoMethodError => e
-            puts "Monotype"
+            #puts "Monotype"
         end
         height = p["height"]
         weight = p["weight"]
@@ -61,9 +72,9 @@ def createPokemonFile(filename, gen)
         File.new("../Resources/#{gen}/#{poke}.txt", "w+")
         File.write("../Resources/#{gen}/#{poke}.txt", statString)
     }
-  file.close
+    file.close
 end
 
-for i in 1..1
+for i in 1..8
     createPokemonFile("../Resources/#{i}.txt", i)
 end
